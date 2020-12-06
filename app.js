ee54a29c4f8d4c35208a7de27ff401034fcaf2b6
Kaustubh-Dengle <dengle.kaustubh@gmail.com>
2020-12-06 16:06:32 +0530

    app ready, designing remaining

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+// referencing all textareas and btns
 var txtInput = document.querySelector("#inputText")
+var btnTranslate = document.querySelector("#translateBtn")
+var txtOutput = document.querySelector("#outputBox")
+
+//api url
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+// changing url by adding input text
+function getNewURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+//click button wiring
+function onBtnClick() {
+    var textInput = txtInput.value
+    fetch(getNewURL(textInput))
+    .then(res => res.json())
+    .then(translation => {
+        var translatedTxt = translation.contents.translated
+        txtOutput.innerText = translatedTxt
+    })
+}
+
+btnTranslate.addEventListener("click", onBtnClick)
